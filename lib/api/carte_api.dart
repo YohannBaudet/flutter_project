cbd543ac37e8e69c7587e39b172993ad97a49c92
Jeremy Fromenteau <o2183468@campus.univ-orleans.fr>
2022-05-05 15:16:02 +0200

    correction de la recherche

--- lib/api/carte_api.dart
+++ lib/api/carte_api.dart
@@ -10,19 +10,20 @@ class Carte {
   });
 
   static Carte fromJson(Map<String, dynamic> json) => Carte(
-    name: json['name'],
+    name : json['name'],
   );
 }
 
 class CarteApi {
   static Future<List<Carte>> getCarteSuggestions(String query) async {
-    final url = Uri.parse('https://db.ygoprodeck.com/api/v7/cardinfo.php?language=fr&num=1&offset=0');
+    final url = Uri.parse('https://db.ygoprodeck.com/api/v7/cardinfo.php?language=fr');
     final response = await http.get(url);
 
 
 
     if (response.statusCode == 200) {
-      final List cartes = json.decode(response.body);
+      var data =json.decode(response.body);
+      var cartes = data["data"] as List;
       return cartes.map((json) => Carte.fromJson(json)).where((carte) {
         final nameLower = carte.name.toLowerCase();
         final queryLower = query.toLowerCase();
