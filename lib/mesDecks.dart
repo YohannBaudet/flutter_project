82adcf3f488f1b98dd56518dd95f53d7e6183fa2
YohannBaudet <yohann.baudet@laposte.net>
2022-05-10 19:04:53 +0200

    fix refesh page mesdecks

--- lib/mesDecks.dart
+++ lib/mesDecks.dart
@@ -124,12 +124,7 @@ class _MesDecksState  extends State<MesDecks>{
                     child: InkWell(
                         onTap: (){
                           preferenceUtils.saveDecks();
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => DeckDetails(deck: preferenceUtils.getDeck[index]),
-                            ),
-                          );
+                          _navigateAndDisplaySelection(context,preferenceUtils.getDeck[index]);
                         },
                         splashColor: Colors.white10, // Splash color over image
                         child: Center(
@@ -152,42 +147,15 @@ class _MesDecksState  extends State<MesDecks>{
     },
   );
   }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String dropdownValue = 'One';
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>['One', 'Two', 'Free', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
+  void _navigateAndDisplaySelection(BuildContext context,Deck deck) async {
+    // Navigator.push returns a Future that completes after calling
+    // Navigator.pop on the Selection Screen.
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => DeckDetails(deck: deck,)),
     );
+    setState((){});
   }
 }
+
 
